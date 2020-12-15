779a783c3561f438784dbfce688622cdd121de33
gk008 <51841775+gk008@users.noreply.github.com>
2020-12-15 20:24:26 +0800

    Revert "根据畅爷的comment修改一些小地方"
    This reverts commit 8a50b3d19d508ed65d9dc1208ebb247c29ae22e6.

--- qmoj/src/test/java/team/akina/qmoj/QmojApplicationTests.java
+++ qmoj/src/test/java/team/akina/qmoj/QmojApplicationTests.java
@@ -17,7 +17,6 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
 @SpringBootTest
 class QmojApplicationTests {
@@ -35,19 +34,11 @@ class QmojApplicationTests {
     LeetCodeHelper leetCodeHelper;
     @Test
     void contextLoads() throws Exception {
-        Logger logger = Logger.getLogger("test");
-        try{
-            for(int i = 0 ;i<leetCodeHelper.getProblemsList().size();i++)
-            {
-                QmojStatStatusPairs zz = leetCodeHelper.getProblemsList().get(i);
-                logger.info("第"+i+"个键值对的值为"+zz.getLevel()+zz.getQuestion__title_slug()+zz.getQuestion_id());
-            }
-        }
-        catch (Exception e)
+        for(int i = 0 ;i<leetCodeHelper.getProblemsList().size();i++)
         {
-            logger.info("报错！错误信息为："+e.toString());
+            QmojStatStatusPairs zz = leetCodeHelper.getProblemsList().get(i);
+            System.out.println("第"+i+"个键值对的值为"+zz.getLevel()+zz.getQuestion__title_slug()+zz.getQuestion_id());
         }
-
 //        List<String> warnings = new ArrayList<String>();
 //        boolean overwrite = true;
 //        File configFile = new File("./src/main/resources/generatorConfig.xml");//直接放在文件下面，与pom.xml同级
