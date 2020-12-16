fa85ea7d2a1d654cdf02a392497e5d7625046320
gk008 <51841775+gk008@users.noreply.github.com>
2020-12-17 00:09:05 +0800

    提交到GK分支

--- qmoj/src/test/java/team/akina/qmoj/QmojApplicationTests.java
+++ qmoj/src/test/java/team/akina/qmoj/QmojApplicationTests.java
@@ -1,15 +1,18 @@
 package team.akina.qmoj;
 
+import lombok.var;
 import org.junit.jupiter.api.Test;
 import org.mybatis.generator.exception.InvalidConfigurationException;
 import org.mybatis.generator.exception.XMLParserException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import team.akina.qmoj.pojo.QmojStatStatusPairs;
+import team.akina.qmoj.pojo.QmojTitleAndContent;
 import team.akina.qmoj.utils.LeetCodeHelper;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.logging.Logger;
 
 @SpringBootTest
 class QmojApplicationTests {
@@ -27,10 +30,13 @@ class QmojApplicationTests {
     LeetCodeHelper leetCodeHelper;
 
     @Test
-    void contextLoads() {
-        for (int i = 0; i < leetCodeHelper.getQuestionsList().size(); i++) {
-            QmojStatStatusPairs zz = leetCodeHelper.getQuestionsList().get(i);
-            System.out.println("第" + i + "个键值对的值为" + zz.getLevel() + zz.getQuestion__title_slug() + zz.getQuestion_id());
+    void contextLoads() throws Exception {
+        Logger logger = Logger.getLogger("test");
+        var list = leetCodeHelper.getTitleAndContentList();
+
+        for (int i = 0; i < list.size(); i++) {
+            QmojTitleAndContent qmojTitleAndContent = list.get(i);
+            logger.info("中文内容为:"+qmojTitleAndContent.getContent()+"中文标题为："+qmojTitleAndContent.getTitle()+"标签数组为："+qmojTitleAndContent.getTopicTags());
         }
     }
 
