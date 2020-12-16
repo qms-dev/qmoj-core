89d89a2559711fe39ff15bfd7f54615339c17f12
GadHao <524472212@qq.com>
2020-12-17 01:09:58 +0800

    恢复通过MyBatisGenerator快速生成相关映射文件的代码

--- qmoj/src/test/java/team/akina/qmoj/QmojApplicationTests.java
+++ qmoj/src/test/java/team/akina/qmoj/QmojApplicationTests.java
@@ -1,15 +1,22 @@
 package team.akina.qmoj;
 
 import org.junit.jupiter.api.Test;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.exception.InvalidConfigurationException;
 import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import team.akina.qmoj.pojo.QmojStatStatusPairs;
 import team.akina.qmoj.utils.LeetCodeHelper;
 
+import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 class QmojApplicationTests {
@@ -34,4 +41,15 @@ class QmojApplicationTests {
         }
     }
 
+    @Test
+    void generateDB() throws IOException, XMLParserException, InvalidConfigurationException, SQLException, InterruptedException {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        File configFile = new File("./src/main/resources/generatorConfig.xml");//直接放在文件下面，与pom.xml同级
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
 }
